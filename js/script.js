790c067b8f2ec2924378d2a9498eb28335d13eca
Natalie <91116178+NatMorrigan@users.noreply.github.com>
2022-02-15 18:22:45 +0100

    12
    практика

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log(1);
+ const numberOfFilms = +prompt('Скільки фільмів ви уже подивилися?', '');
+
+ const personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     actors: {},
+     genres: [],
+     privat: false 
+    }; 
+
+    const a = prompt('Один з останніх переглянутих фільмів?', ''),
+          b = prompt('На скільки оцінюєте його?',''),
+          c = prompt('Один з останніх переглянутих фільмів?', ''),
+          d = prompt('На скільки оцінюєте його?','');
+
+   personalMovieDB.movies[a] = b;
+   personalMovieDB.movies [c] = d;
+
+   console.log(personalMovieDB);
